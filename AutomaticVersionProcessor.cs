8b25b16d494e8cba7eb5e3fb40d8d87427266a7d
Goo <kuwoods@hotmail.com>
2020-09-18 14:30:31 +0800

    Implement AutomaticVersionProcessor.

---
+++ AutomaticVersionProcessor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor;
+
+public class AutomaticVersionProcessor : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+#if UNITY_ANDROID
+        //for google request
+        PlayerSettings.Android.targetArchitectures |= AndroidArchitecture.ARM64;
+
+        if (EditorUtility.DisplayDialog("Bundle Version Code"
+            , $"Current Bundle Version Code:{PlayerSettings.Android.bundleVersionCode}, ++?"
+            , "Yes", "No"))
+        {
+            PlayerSettings.Android.bundleVersionCode++;
+        }
+#endif
+    }
+}
